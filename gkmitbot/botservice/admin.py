c58eee48c701f676a497e693c1ef757bee1cb75e
kaustubh-tr <trivedikaustubh01@gmail.com>
2024-01-29 19:00:07 +0530

    added api

--- gkmitbot/botservice/admin.py
+++ gkmitbot/botservice/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register([Employee, Skill, Emp_Skill, Project, Emp_Project, Chat_History])
--- gkmitbot/gkmitbot/urls.py
+++ gkmitbot/gkmitbot/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import path
 from botservice import views
 from django.conf.urls import include
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/",include("botservice.urls")),
+    path("",include("botservice.urls")),
 ]
